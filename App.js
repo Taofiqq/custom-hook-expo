c9cae33760991f24dfca00850cbacb0d4e8bd883
Taofiqq <taofiqaiyelabegan45@gmail.com>
2023-11-04 22:07:56 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Raleway_200ExtraLight } from "@expo-google-fonts/raleway";
+import { Quicksand_300Light } from "@expo-google-fonts/quicksand";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Raleway_200ExtraLight,
+    Quicksand_300Light,
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This text has default style</Text>
+      <Text style={styles.raleway}>This text uses Raleway Font</Text>
+      <Text style={styles.quicksand}>This text uses QuickSand Font</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  raleway: {
+    fontSize: 20,
+    fontFamily: "Raleway_200ExtraLight",
+  },
+  quicksand: {
+    fontSize: 20,
+    fontFamily: "Quicksand_300Light",
   },
 });
